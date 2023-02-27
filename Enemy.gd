90f5584b474f75c02cb5a8e0bd0d3f15961bf5e0
Michael <117549863+Seadra26@users.noreply.github.com>
2023-02-27 02:24:16 -0600

    added knockback, health system, sprite animations, crab enemy

--- Enemy.gd
+++ Enemy.gd
@@ -1,26 +1,61 @@
 extends KinematicBody2D
 
 var motion = Vector2()
-
+var knockback_direction
+var knockback_delay = 1000
+var knockback = false
+var direction = 1
+var health = 2
+var speed = 4
+var move = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite.play()
 
 func _physics_process(_delta):
-	var Player = get_parent().get_node("Player")
-	
-	position += (Player.position - position)/100  # dividing this number (currently 50) by smaller numbers will make the enemies approach the player at a faster speed.
-	look_at(Player.position)
-	
-	move_and_collide(motion)
-
+	if move:
+		var Player = get_parent().get_node("Player")
+		
+		motion = position.direction_to(Player.position) * speed
+		if motion.x < 0:
+			$AnimatedSprite.flip_h = true
+			direction = -1
+		else:
+			$AnimatedSprite.flip_h = false
+			direction = 1
+			
+		if knockback:
+			#should be knockback_direction vv
+			motion.x = 150 * knockback_direction
+			knockback = false
+			knockback_delay = 1000
+			
+		knockback_delay -= 1
+		
+		move_and_collide(motion)
+		
+		
+func knockback():
+	var enemy_direction = get_parent().get_node("Player").direction
+	knockback_direction = enemy_direction
+	knockback = true
 
 func _on_HitboxArea_body_entered(body):
 	if "Knife" in body.name:
-		queue_free()
+		health -= 1
+		knockback()
+		if health <= 0:
+			queue_free()
 		body.queue_free()
-
 
 func _on_HitboxArea_area_entered(area):
 	if "meleehit" in area.name:
-		queue_free()
+		health -= 1
+		knockback()
+		if health <= 0:
+			queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	if "Player" in body.name:
+		move = true
--- Player.gd
+++ Player.gd
@@ -1,8 +1,16 @@
 extends KinematicBody2D
 
-var movespeed = 600
-var direction
+var max_hearts = 5
+var num_hearts = 5
 
+var knockback_direction
+var knockback_delay = 40
+var knockback = false
+var movespeed = 600 * 60
+var extravelo = Vector2()
+var in_hitbox = false
+var body_in_hitbox
+var direction = 1
 
 export var jump_height : float
 export var jump_peak_t : float
@@ -12,14 +20,18 @@ onready var jump_grav : float = ((-2.0 * jump_height) / (jump_peak_t * jump_peak
 onready var jump_velo : float = ((2.0 * jump_height) / jump_peak_t) * -1.0
 onready var fall_grav : float = ((-2.0 * jump_height) / (jump_desc_t * jump_desc_t)) * -1.0
 
-var speed = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	 $AudioStreamPlayer.play()
+	$AudioStreamPlayer.play()
+	$gamecamera/UI/life/hearts.rect_size.x = max_hearts * 16
+#	connect("life_changed", $gamecamera/UI/life/hearts, "on_life_change")
+#	emit_signal("life_changed", max_hearts)
 
 var velocity = Vector2.ZERO
+
 func _physics_process(delta):
+	print(delta)
 	velocity.x = 0
 	var gravity = fall_grav
 	if velocity.y < 0.0:
@@ -28,34 +40,75 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		direction = -1
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	velocity.x *= movespeed
-	
-#	if velocity.y != 0 and is_on_floor():
-#		$AnimatedSprite.animation = 'jump'
-	if velocity.x != 0:
-		$AnimatedSprite.animation = 'run'
-		$AnimatedSprite.play()
-		if velocity.x < 0:
-			$AnimatedSprite.flip_h = true
-		else:
-			$AnimatedSprite.flip_h = false
+		direction = 1
+	velocity.x *= movespeed * delta
+	if velocity.x < 0:
+		$AnimatedSprite.flip_h = true
+	elif velocity.x > 0:
+		$AnimatedSprite.flip_h = false
+			
+	if velocity.y != 0 and not is_on_floor():
+		$AnimatedSprite.play("jump")
+	elif velocity.x != 0:
+		$AnimatedSprite.play("run")
+		
 	else:
-		$AnimatedSprite.stop()
-		$AnimatedSprite.frame = 0
-		$AnimatedSprite.animation = 'idle'
-
+		$AnimatedSprite.play("idle")
+	
+#	$AnimatedSprite.play()
+	
+	if knockback_delay <= 0 and in_hitbox:
+		damage()
+		knockback(body_in_hitbox)
+		knockback_delay = 40
+		
+	knockback_delay -= 1
+	
+	
+	if knockback:
+		velocity.y += -400
+		velocity.x += 10000 * knockback_direction
+		knockback = false
+		
+			
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_velo
-	velocity = move_and_slide(velocity, Vector2.UP)
+		
+	if knockback:
+		extravelo.y = -500 * movespeed
+		extravelo.x = 500 * movespeed * knockback_direction * -1
+		velocity = move_and_slide(velocity + extravelo, Vector2.UP)
+		extravelo = lerp(extravelo, Vector2.ZERO, 0.9)
+	else:
+		velocity = move_and_slide(velocity, Vector2.UP)
+	
+	
 	
 
 func kill():
 	get_tree().reload_current_scene()  # this will likely be changed, as this is to make the game reload every time the player dies.
+func knockback(name):
+	var enemy_direction = get_parent().get_node("{enemy}".format({"enemy": name})).direction
+	knockback_direction = enemy_direction
+	knockback = true
+	
+	
+func damage():
 
+	num_hearts -= 1
+	$gamecamera/UI/life/hearts.rect_size.x = num_hearts * 16
+	if num_hearts == 0:
+		kill()
+	
 func _on_HitboxArea_body_entered(body):
-	if "Enemy" in body.name:
-		kill()
-	if "Tomato" in body.name:
-		kill()
+	if "enemy" in body.name:
+		in_hitbox = true
+		body_in_hitbox = body.name
+		
+
+func _on_HitboxArea_body_exited(body):
+	if "enemy" in body.name:
+		in_hitbox = false
